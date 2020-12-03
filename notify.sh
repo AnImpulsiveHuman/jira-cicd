f49b3f9789fb0b3ea40c9ad09c3d70832d087c5d
vishalpranav03 <64655668+vishalpranav03@users.noreply.github.com>
2020-12-03 16:31:18 +0530

    Create notify.sh

---
+++ notify.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl --request POST --url $U --user $AUTH --header $H1 --header $H2 --data '{"fields":{"summary":"CodeBuild - Build Failed","issuetype":{"name":"Bug"},"project":{"key":"TCP"},"description":{"type":"doc","version":1,"content":[{"type":"paragraph","content":[{"text":"Build ID:'$CODEBUILD_BUILD_ID' \n Build Number:'$CODEBUILD_BUILD_NUMBER'","type":"text"}]}]}}}'
